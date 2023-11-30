544c15714f19d9b4f62a4532fb4a5b29ff37b949
Rogier Velting <rogiervelting@gmail.com>
2023-11-30 12:27:54 +0100

    switched final build step back to more thoroughly type checked compiler

--- src/db/cart.ts
+++ src/db/cart.ts
@@ -1,15 +1,16 @@
 import { database } from "./common.js"
-import { Cart } from "../models/cart.js";
+import Cart from "../gql/cart.js";
 
 const cart = database.collection<Cart>('cart');
 
-async function get({userId}) {
+async function get({userId}) : Promise<Cart> {
     return cart.findOne<Cart>({userId});
 }
 
 async function put({userId, prodId}) {
-    let current = get(userId);
-    
+    let current = await get(userId);
+    if (current === null) cart.insertOne({userId: userId, products: [prodId]});
+    if (current.products.filter(p => p.id === prodId).length > 0) return;
     // cart.updateOne({userId}, )
 }
 
